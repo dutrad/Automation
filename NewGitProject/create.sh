93440268d5e77c9faf636836a2926ab64d9472a2
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-05-14 15:36:56 +0100

    Custom alias

---
+++ NewGitProject/create.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+function create(){
+     echo 'fsdfsa'
+}
+cd ~/Documents/GitHub/
+python ~/Documents/GitHub/Automation/NewGitProject/create.py $1
+mkdir $1
+cd $1
+#git init
+#echo "# $1" >> README.md
+#git add .
+#git commit -m "Initial commit"
+#git remote add origin https://github.com/dutrad/$1.git
+#git push -u origin master
