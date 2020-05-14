19536ce44dc2105a6bf6dfcbbe98f17b49b4dfb3
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-05-14 17:54:19 +0100

    Update github.py and create.sh

--- NewGitProject/create.sh
+++ NewGitProject/create.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-function create(){
-     echo 'fsdfsa'
-}
 cd ~/Documents/GitHub/
-python ~/Documents/GitHub/Automation/NewGitProject/create.py $1
+python ~/Documents/GitHub/Automation/NewGitProject/github.py $1
 mkdir $1
 cd $1
-#git init
-#echo "# $1" >> README.md
-#git add .
-#git commit -m "Initial commit"
-#git remote add origin https://github.com/dutrad/$1.git
-#git push -u origin master
+git init
+echo "# $1" >> README.md
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/dutrad/$1.git
+git push -u origin master
+code .
--- NewGitProject/github.py
+++ NewGitProject/github.py
@@ -1,1 +1,15 @@
-print('Hello Galo')
+import sys
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://github.com/login')
+
+def create():
+    projectName = str(sys.argv[1])
+    print(projectName)
+    button = browser.find_element_by_xpath("/html/body/div[3]/main/div/form/div[4]/input[9]")
+    button.click()
+
+if __name__ == "__main__":
+    create()
+
