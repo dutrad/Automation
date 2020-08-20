e4958d7d6c89fa635304bb1b543fc15df10d7733
dutrad <34178422+dutrad@users.noreply.github.com>
2020-08-20 10:22:00 +0100

    Get filename from cmd argument

--- ScanNetwork/scan.py
+++ ScanNetwork/scan.py
@@ -1,5 +1,6 @@
 import subprocess
 import datetime
+import sys
 
 IP_NETWORK = '192.168.1.'
 
@@ -11,7 +12,8 @@ def checkIp(ip):
 
 def getNames():
   d = {}
-  with open("ips.txt") as f:
+  fileName = sys.argv[1]
+  with open(fileName) as f:
     for line in f:
       (key, val) = line.split()
       d[int(key)] = val
@@ -35,14 +37,3 @@ while True:
           msg = now.strftime("%c") + " " + str(name) + " online\n"
       else:
           msg = now.strftime("%d/%m/%Y %H:%M:%S") + " " + str(name) + " offline\n"
-
-      with open('out.txt', 'a+') as file:
-          file.write(msg)
-
-
-
-
-
-
-
-
