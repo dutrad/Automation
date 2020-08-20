b4e0393cce708e06ed402e5890c0d56c6a3c93d6
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-08-20 11:13:12 +0100

    Get out file by cmd and refactor

--- ScanNetwork/scan.py
+++ ScanNetwork/scan.py
@@ -4,39 +4,43 @@ import sys
 
 IP_NETWORK = '192.168.1.'
 
+
 def checkIp(ip):
-  ip = IP_NETWORK + str(ip)
-  resp = subprocess.call(['ping', '-c', '1', ip], stdout=subprocess.DEVNULL)
+    ip = IP_NETWORK + str(ip)
+    out = subprocess.call(['ping', '-c', '1', ip], stdout=subprocess.DEVNULL)
 
-  return (resp == 0)
+    return out == 0
 
-def getNames():
-  d = {}
-  fileName = sys.argv[1]
-  with open(fileName) as f:
-    for line in f:
-      (key, val) = line.split()
-      d[int(key)] = val
-  return d
 
-names = getNames()
+def getnames():
+    d = {}
+    file = sys.argv[1]
+    with open(file) as f:
+        for line in f:
+            (key, val) = line.split()
+            d[int(key)] = val
+    return d
+
+
+names = getnames()
 connected = [False] * 100
+fileName = sys.argv[2]
 
 while True:
-  for i in range(64,100):
-    resp = checkIp(i)
+    for i in range(64, 100):
+        resp = checkIp(i)
 
-    if resp != connected[i]:
-      connected[i] = resp
-      name = names.get(i)
-      msg = ""
-      now = datetime.datetime.now()
-      if not name:
-          name = i
-      if resp:
-          msg = now.strftime("%c") + " " + str(name) + " online\n"
-      else:
-          msg = now.strftime("%d/%m/%Y %H:%M:%S") + " " + str(name) + " offline\n"
-      
-      with open('out.txt', 'a+') as file:
-          file.write(msg)
+        if resp != connected[i]:
+            connected[i] = resp
+            name = names.get(i)
+            msg = ""
+            now = datetime.datetime.now()
+            if not name:
+                name = i
+            if resp:
+                msg = now.strftime("%c") + " " + str(name) + " online\n"
+            else:
+                msg = now.strftime("%d/%m/%Y %H:%M:%S") + " " + str(name) + " offline\n"
+
+            with open(fileName, 'a+') as outFile:
+                outFile.write(msg)
