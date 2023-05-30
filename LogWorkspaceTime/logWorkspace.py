49d6dadc9459f22f9fc44f611f6a3f92e8c792a2
Vinicius Dias <dutrad.vinicius@gmail.com>
2023-05-30 19:44:28 +0100

    Save time by day

--- LogWorkspaceTime/logWorkspace.py
+++ LogWorkspaceTime/logWorkspace.py
@@ -1,28 +1,49 @@
 import os
+import json
 from time import sleep
-from datetime import timedelta
+from datetime import timedelta, date
 
 WORKSPACE_CMD = "wmctrl -d | grep '*' | cut -d ' ' -f1"
-TIME_FILE = 'workspacetime.txt'
+TIME_FILE = 'workspacetime.json'
 SLEEP_TIME = 2
+DISPLAY_FORMAT = 'Today: {}     Week: {}'
+
 
 def increment_time():
-    time = '1'
+    time_json = {}
+    today = date.today().strftime("%d-%m-%Y")
     if os.path.isfile(TIME_FILE):
         with open(TIME_FILE, 'r') as file:
-            time = file.readline().strip()
+            time_json = json.load(file)
 
-    time_int = int(time) + SLEEP_TIME
+    time_json[today] = time_json.get(today, 0) + SLEEP_TIME
     with open(TIME_FILE, 'w') as file:
-        file.write(str(time_int)+'\n')
+        json.dump(time_json, file)
 
-    print(str(timedelta(seconds=time_int)), end='\r')
-    
+    show_total(time_json)
+
+
+def show_total(time_json):
+    today = date.today()
+    dates = [today + timedelta(days=i)
+             for i in range(0 - today.weekday(), 7 - today.weekday())]
+
+    total = 0
+    for day in dates:
+        total += time_json.get(day.strftime("%d-%m-%Y"), 0)
+
+    display_str = DISPLAY_FORMAT.format(str(
+        timedelta(seconds=time_json[today.strftime("%d-%m-%Y")])), str(timedelta(seconds=total)))
+
+    print(display_str, end='\r')
+
+
 def main():
     while True:
         if os.popen(WORKSPACE_CMD).read().strip() == '1':
             increment_time()
         sleep(SLEEP_TIME)
 
+
 if __name__ == "__main__":
-   main()
+    main()
